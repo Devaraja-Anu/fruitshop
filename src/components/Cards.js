826b7592ec151701f54f7f6843d153a8e0006ead
Anu <anubusinesscc8@gmail.com>
2022-12-09 14:40:03 +0530

    cards reverted to static

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,90 +1,90 @@
-import oranges from "../images/oranges.jpg";
-import strawberries from "../images/strawberries.jpg";
-import grapes from "../images/grapes.jpg";
-import lemons from "../images/lemons.jpg";
+// import oranges from "../images/oranges.jpg";
+// import strawberries from "../images/strawberries.jpg";
+// import grapes from "../images/grapes.jpg";
+// import lemons from "../images/lemons.jpg";
 import appleGreen from "../images/greenApples.jpg";
 import appleRed from "../images/RedApples.jpg";
-import pears from "../images/pears.jpg";
-import kiwis from "../images/kiwis.jpg";
-import mangoes from "../images/mangoes.jpg";
-import raspberries from "../images/raspberries.jpg";
-import pineapples from "../images/pineapples.jpg";
-import blueberries from "../images/blueberries.jpg";
-import lychee from "../images/lychee.jpg";
-import figs from "../images/figs.jpg";
-import blackberries from "../images/blackberries.jpg";
-import guava from "../images/guava.jpg";
+// import pears from "../images/pears.jpg";
+// import kiwis from "../images/kiwis.jpg";
+// import mangoes from "../images/mangoes.jpg";
+// import raspberries from "../images/raspberries.jpg";
+// import pineapples from "../images/pineapples.jpg";
+// import blueberries from "../images/blueberries.jpg";
+// import lychee from "../images/lychee.jpg";
+// import figs from "../images/figs.jpg";
+// import blackberries from "../images/blackberries.jpg";
+// import guava from "../images/guava.jpg";
 import placeholder from "../images/productPlaceholder.jpg"
-import { useState, useEffect } from "react";
-import axios from "axios";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
 
-const fruitnames = [
-  {
-    fruit: "appleRed",
-    image: appleRed,
-  },
-  {
-    fruit: "appleGreen",
-    image: appleGreen,
-  },
-  {
-    fruit: "oranges",
-    image: oranges,
-  },
-  {
-    fruit: "strawberries",
-    image: strawberries,
-  },
-  {
-    fruit: "grapes",
-    image: grapes,
-  },
-  {
-    fruit: "lemons",
-    image: lemons,
-  },
+// const fruitnames = [
+//   {
+//     fruit: "appleRed",
+//     image: appleRed,
+//   },
+//   {
+//     fruit: "appleGreen",
+//     image: appleGreen,
+//   },
+//   {
+//     fruit: "oranges",
+//     image: oranges,
+//   },
+//   {
+//     fruit: "strawberries",
+//     image: strawberries,
+//   },
+//   {
+//     fruit: "grapes",
+//     image: grapes,
+//   },
+//   {
+//     fruit: "lemons",
+//     image: lemons,
+//   },
 
-  {
-    fruit: "pears",
-    image: pears,
-  },
-  {
-    fruit: "kiwis",
-    image: kiwis,
-  },
-  {
-    fruit: "mangoes",
-    image: mangoes,
-  },
-  {
-    fruit: "raspberries",
-    image: raspberries,
-  },
-  {
-    fruit: "pineapples",
-    image: pineapples,
-  },
-  {
-    fruit: "blueberries",
-    image: blueberries,
-  },
-  {
-    fruit: "lychee",
-    image: lychee,
-  },
-  {
-    fruit: "guava",
-    image: guava,
-  },
-  {
-    fruit: "figs",
-    image: figs,
-  },
-  {
-    fruit: "blackberries",
-    image: blackberries,
-  },
-];
+//   {
+//     fruit: "pears",
+//     image: pears,
+//   },
+//   {
+//     fruit: "kiwis",
+//     image: kiwis,
+//   },
+//   {
+//     fruit: "mangoes",
+//     image: mangoes,
+//   },
+//   {
+//     fruit: "raspberries",
+//     image: raspberries,
+//   },
+//   {
+//     fruit: "pineapples",
+//     image: pineapples,
+//   },
+//   {
+//     fruit: "blueberries",
+//     image: blueberries,
+//   },
+//   {
+//     fruit: "lychee",
+//     image: lychee,
+//   },
+//   {
+//     fruit: "guava",
+//     image: guava,
+//   },
+//   {
+//     fruit: "figs",
+//     image: figs,
+//   },
+//   {
+//     fruit: "blackberries",
+//     image: blackberries,
+//   },
+// ];
 
 const DB = [
   {
@@ -105,27 +105,27 @@ const defaultBackground = 'bg-background'
 
 const CardScroll = () => {
 
-  const [database, setdatabase] = useState([]);
+  // const [database, setdatabase] = useState([]);
 
-  useEffect(() => {
-    axios.get("http://localhost:3000/products").then((response) => {
-      setdatabase(response.data)
-    });
-  }, []);
+  // useEffect(() => {
+  //   axios.get("http://localhost:3000/products").then((response) => {
+  //     setdatabase(response.data)
+  //   });
+  // }, []);
 
-  const findImage = (fruitName) => {
-    const selectedFruit = fruitnames.find((fruit) => fruit.fruit === fruitName);
-    return selectedFruit?.image;
-  };
+  // const findImage = (fruitName) => {
+  //   const selectedFruit = fruitnames.find((fruit) => fruit.fruit === fruitName);
+  //   return selectedFruit?.image;
+  // };
 
-  return database.map((details ) => {
+  return DB.map((details ) => {
 
     return (
       <div key={details.image} className="h-96 w-64 lg:my-4">
         <div className=" border-2 rounded-3xl ">
           <img
             className="rounded-t-3xl object-cover h-48 w-fit "
-            src={findImage ?? placeholder}
+            src={details.image ?? placeholder}
             alt="A sample of the product is displayed "
           />
           <div className="flex flex-col justify-center items-center">
